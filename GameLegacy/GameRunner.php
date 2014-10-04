fe05c50d4980cefa0e125e861814fb98785bccde
jeslopcru <jeslopcru@gmail.com>
2014-10-04 14:13:40 +0200

    code style

--- GameLegacy/GameRunner.php
+++ GameLegacy/GameRunner.php
@@ -1,28 +1,25 @@
 <?php
 
-include __DIR__.'/Game.php';
+include __DIR__ . '/Game.php';
 
 $notAWinner;
 
-  $aGame = new Game();
-  
-  $aGame->add("Chet");
-  $aGame->add("Pat");
-  $aGame->add("Sue");
-  
-  
-  do {
-    
-    $aGame->roll(rand(0,5) + 1);
-    
-    if (rand(0,9) == 7) {
-      $notAWinner = $aGame->wrongAnswer();
+$aGame = new Game();
+
+$aGame->add("Chet");
+$aGame->add("Pat");
+$aGame->add("Sue");
+
+do {
+
+    $aGame->roll(rand(0, 5) + 1);
+
+    if (rand(0, 9) == 7) {
+        $notAWinner = $aGame->wrongAnswer();
     } else {
-      $notAWinner = $aGame->wasCorrectlyAnswered();
+        $notAWinner = $aGame->wasCorrectlyAnswered();
     }
-    
-    
-    
-  } while ($notAWinner);
+
+} while ($notAWinner);
   
 
