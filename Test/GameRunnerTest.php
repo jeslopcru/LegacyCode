c8dc143af7a7cb853a26be7d9c8efb707ef915e4
jeslopcru <jeslopcru@gmail.com>
2014-10-06 19:52:59 +0200

    first test

--- Test/GameRunnerTest.php
+++ Test/GameRunnerTest.php
@@ -1,16 +1,24 @@
 <?php
+require_once __DIR__ . '/../GameLegacy/GameRunner.php';
 
 class GameRunnerTest extends PHPUnit_Framework_TestCase
 {
 
     public function testOutput()
     {
+        file_put_contents('/tmp/LegacyGameOutput.txt', $this->generateOutput());
+        $file_content = file_get_contents('/tmp/LegacyGameOutput.txt');
+        $this->assertEquals($file_content, $this->generateOutput());
+    }
+
+    private function generateOutput()
+    {
         ob_start();
-        require_once __DIR__ . '/../GameLegacy/GameRunner.php';
+        srand(0);
         $output = ob_get_contents();
         ob_end_clean();
 
-        var_dump($output);
+        return $output;
     }
 
 }
