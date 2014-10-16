f227e7f5c3afb78943fc83233692bcb4962a33db
jeslopcru <jeslopcru@gmail.com>
2014-10-16 20:06:22 +0200

    refactor test

--- Test/GameTest.php
+++ Test/GameTest.php
@@ -3,9 +3,28 @@ require __DIR__ . '/../vendor/autoload.php';
 
 class GameTest extends PHPUnit_Framework_TestCase
 {
+    /** @var  Game */
+    public $_game;
+
+    public function setUp()
+    {
+        $this->_game = new Game();
+    }
+
     public function testCreateAGameOk()
     {
-        $game = new Game();
-        $this->assertInstanceOf('Game', $game);
+        $this->assertInstanceOf('Game', $this->_game);
+    }
+
+    public function testAJustCreatedNewGameIsNotPlayable()
+    {
+        $this->assertFalse($this->_game->isPlayable());
+    }
+
+    public function testCreatedNewGameIsPlayable()
+    {
+        $this->_game->add('player 1');
+        $this->_game->add('player 2');
+        $this->assertTrue($this->_game->isPlayable());
     }
 }
